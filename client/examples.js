fb6603a1eaa4d8d92fd534a6943037bce607c375
Oliinyk Yulia <Lia.Oliynyk6@gmail.com>
2021-12-06 22:29:44 +0200

    Add new example

--- client/examples.js
+++ client/examples.js
@@ -13,15 +13,30 @@ client.listOfBalancers()
         console.log(list);
     })
     .catch((e) => {
-        console.log(`Problem listing available balancers: ${e.message}`);
+        console.log(Problem listing available balancers: ${e.message});
     });
 
-// Scenario 2: Update a machine.
-client.updateMachine(3, true)
-    .then((resp) => {
-        console.log('=== Scenario 2 ===');
-        console.log('Update machine status response:', resp);
-    })
-    .catch((e) => {
-        console.log(`Problem updating a machine: ${e.message}`);
-    });
+setTimeout(() => {
+    // Scenario 2: Update a machine.
+    client.updateMachine(2, false)
+        .then((resp) => {
+            console.log('=== Scenario 2 ===');
+            console.log('Update machine status response:', resp);
+        })
+        .catch((e) => {
+            console.log(Problem updating a machine: ${e.message});
+        });
+}, 2000)
+
+setTimeout(() => {
+    // Scenario 3: Display available balancers.
+    client.listOfBalancers()
+        .then((list) => {
+            console.log('=== Scenario 1 ===');
+            console.log('Available balancers after update machine:');
+            console.log(list);
+        })
+        .catch((e) => {
+            console.log(Problem listing available balancers: ${e.message});
+        });
+}, 3000)
