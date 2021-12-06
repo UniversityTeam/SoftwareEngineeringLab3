0a112a367f75cb3e3a682560af04523d8b470361
Professor108 <panstepan13@i.ua>
2021-12-06 21:39:19 +0200

    Add displaying used machines in balancer by machine status

--- server/balancers/data.go
+++ server/balancers/data.go
@@ -2,20 +2,25 @@ package balancers
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 )
 
 type Balancer struct {
-	Id   int64  `json:"id"`
-	Name string `json:"name"`
-	UsedMachines []int64 `json:"usedMachines"`
-	TotalMachinesCount int64 `json:"totalMachinesCount"`
+	Id int64  json:"id"
+	Name string json:"name"
+	TotalMachinesCount int64 json:"totalMachinesCount"
+	UsedMachines string json:"usedMachines"
 }
 
 type Machine struct {
-	Id   int64  `json:"id"`
-	Name string `json:"name"`
-	Worked bool `json:"worked"`
+	Id int64  json:"id"
+	Name string json:"name"
+	Worked bool json:"worked"
+}
+
+type CheckResult struct {
+	Exists bool  json:"exists"
 }
 
 type Store struct {
@@ -37,7 +42,7 @@ func (s *Store) ListBalancers() ([]*Balancer, error) {
 	var res []*Balancer
 	for rows.Next() {
 		var c Balancer
-		if err := rows.Scan(&c.Id, &c.Name); err != nil {
+		if err := rows.Scan(&c.Id, &c.Name, &c.TotalMachinesCount, &c.UsedMachines); err != nil {
 			return nil, err
 		}
 		res = append(res, &c)
@@ -45,6 +50,36 @@ func (s *Store) ListBalancers() ([]*Balancer, error) {
 	if res == nil {
 		res = make([]*Balancer, 0)
 	}
+
+	for i := 0; i < len(res); i++ {
+		var machines []int
+		var workedMachines []int
+		json.Unmarshal([]byte(res[i].UsedMachines), &machines)
+
+		for j := 0; j < len(machines); j++ {
+			check, _ := s.Db.Query("select exists(select 1 from \"machines\" where id=$1 AND worked=true)", machines[j])
+			var checkRes []*CheckResult
+			for check.Next() {
+				var c CheckResult
+				if err := check.Scan(&c.Exists); err != nil {
+					return nil, err
+				}
+				checkRes = append(checkRes, &c)
+			}
+
+			if checkRes[0].Exists {
+				workedMachines = append(workedMachines, machines[j])
+			}
+		}
+
+		correctData, err := json.Marshal(workedMachines)
+		if err != nil {
+			return nil, err
+		}
+		res[i].UsedMachines = string(correctData)
+
+	}
+
 	return res, nil
 }
 
